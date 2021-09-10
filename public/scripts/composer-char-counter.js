e3f5423b4c45e74a2374e93638df1f259af2fb9c
Polina Skrobot <chi.polly.no@gmail.com>
2021-09-10 04:16:37 +0000

    Bugs fixed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-undef */
 $(document).ready(function() {
-  $("#tweet-text").on('keyup', function() {
-    let $counter = $(".counter");
+  $("#tweet-text").on('input', function() {
+    const $counter = $(".counter");
     const maxLength = 140;
-    let currentLength = $(this).val().length;//dynamical length of input
+    const currentLength = $(this).val().length;//dynamical length of input
     $counter.val(maxLength - currentLength);//dynamically change last num of sym
     if (currentLength > maxLength) {//check length to change color
       $counter.css("color", "red");
-    } else {
-      $counter.css("color", "#5e6572");
+      return;
     }
+    $counter.css("color", "#5e6572");
+    
   });
 
 });
